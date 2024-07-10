334692fbe3376e1dfb2fd46e081cad5269dea6d3
noor uzma <uzmasalmaan23@gmail.com>
2024-07-10 09:27:46 +0530

    implemented the dropdows

---
+++ xstates/src/App.js
@@ -0,0 +1,13 @@
+import DisplayLocation from "./component/DisplayLocation";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayLocation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xstates/src/component/DisplayLocation.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./DisplayLocation.module.css"
+ 
+function DisplayLocation(){
+
+    const[countries,setCountries]=useState([]);
+    const[states,setState]=useState([]);
+    const[cities,setCity]=useState([]);
+    const[selectedCountry,setSelectedCountry]=useState("");
+    const[selectedState,setSelectedState]=useState("");
+    const[selectedCity,setSelectedCity]=useState("");
+
+   
+        useEffect(()=>{
+          axios.get("https://crio-location-selector.onrender.com/countries")
+          .then((response)=>{
+            setCountries(response.data)
+          })
+          .catch((error)=>{
+            console.log("Error fetching countries:",error)
+          });
+            
+        },[])
+
+        useEffect(()=>{
+            if(selectedCountry){
+         axios.get(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+         .then((response)=>{
+            setState(response.data);
+            selectedState("");
+            setCity([]);
+            setSelectedCity("");
+
+         })
+         .catch((error)=>{
+            console.log("Error fetching states:",error)
+         })
+            }
+        }, [selectedCountry]);
+
+        useEffect(()=>{
+            if(selectedCountry && selectedState){
+                axios.get(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
+                .then((response)=>{
+               setCity(response.data);
+               setSelectedCity("")
+                })
+                .catch((error)=>{
+                    console.log("Error fetching cities:",error)
+                })
+            }
+        },[selectedCountry,selectedState])
+    
+
+ return(
+    <div>
+        <div className={styles.heading}>
+            <h1>Select Location</h1>
+        </div>
+        <div className={styles.dropdowns}>
+            <select value={selectedCountry} onChange={(e)=>setSelectedCountry(e.target.value)} className={styles.dropdown}>
+                <option value="" disabled>Select Country</option>
+
+                {countries.map((country)=>(
+                <option key={country} value={country}>{country}</option>
+                ))}
+            </select>
+           
+            <select value={selectedState} onChange={(e)=>setSelectedState(e.target.value)} disabled={!selectedCountry} className={styles.dropdown}>
+                <option value="" disabled>Select States</option>
+               {states.map((state)=>(
+                <option key={state} value={state}>{state}</option>
+               ))}
+            </select>
+
+            <select value={selectedCity} onChange={(e)=>setSelectedCity(e.target.value)} disabled={!selectedState} className={styles.dropdown}>
+                <option value="" disabled>Select City</option>
+
+                {cities.map((city)=>(
+                    <option key={city} value={city}>{city}</option>
+                ))}
+            </select>
+
+        </div>
+        {selectedCity &&(
+            <h2 className={styles.result}>
+                You selected  <span className={styles.highlight}> {selectedCity},</span>
+                <span className={styles.fade}>
+                    
+                    {selectedState}, {selectedCountry}
+                </span>
+            </h2>
+        )
+            
+        }
+    </div>
+  )  
+}
+export default DisplayLocation;
